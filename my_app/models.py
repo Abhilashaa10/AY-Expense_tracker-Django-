bfcb032d3aa376566ffee10f9c121007c8afaf63
Abhilasha sarode <sarodeabhilasha057@gmail.com>
2026-06-17 15:02:13 +0530

    expenses initialized

---
+++ my_app/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Expense(models.Model):
+    title=models.CharField(max_length=100)
+    amount = models.IntegerField()
+    category=models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.title
