c69f21c26aa17fb1fe0eda6b3bbe702e3b47a4f6
Abhilasha sarode <sarodeabhilasha057@gmail.com>
2026-06-26 22:24:19 +0530

    create table in model file

---
+++ expenses_app/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class Expense(models.Model):
+    CATEGORY_CHOICES = [
+        ('food', 'Food'),
+        ('transport', 'Transport'),
+        ('rent', 'Rent'),
+        ('utilities', 'Utilities'),
+        ('entertainment', 'Entertainment'),
+        ('other', 'Other'),
+    ]
+
+    title = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='other')
+    date = models.DateField()
+    note = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.amount}"
--- my_app/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Expense(models.Model):
-    title=models.CharField(max_length=100)
-    amount = models.IntegerField()
-    category=models.CharField(max_length=100)
-    
-    # def __str__(self):
-    #     return self.title
