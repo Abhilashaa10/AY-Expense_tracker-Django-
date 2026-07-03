4672ceec70803a3fba70a44c1493682dbf8eba5b
Abhilasha sarode <sarodeabhilasha057@gmail.com>
2026-07-03 17:26:53 +0530

    update http request

--- expenses_app/views.py
+++ expenses_app/views.py
@@ -1,17 +1,16 @@
-from rest_framework.decorators import api_view ,  permission_classes
-from rest_framework.permissions import AllowAny
-from .serializers import RegisterSerializer
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
 from .models import Expense
-from .serializers import ExpenseSerializer
+from .serializers import ExpenseSerializer, RegisterSerializer
 
 
 @api_view(['GET', 'POST'])
 def expense_list(request):
     if request.method == 'GET':
-        expenses = Expense.objects.all().order_by('-date')
+        expenses = Expense.objects.filter(user=request.user).order_by('-date')
 
         category = request.GET.get('category')
         date = request.GET.get('date')
@@ -27,14 +26,14 @@ def expense_list(request):
     elif request.method == 'POST':
         serializer = ExpenseSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def expense_detail(request, pk):
-    expense = get_object_or_404(Expense, pk=pk)
+    expense = get_object_or_404(Expense, pk=pk, user=request.user)
 
     if request.method == 'GET':
         serializer = ExpenseSerializer(expense)
@@ -50,7 +49,6 @@ def expense_detail(request, pk):
     elif request.method == 'DELETE':
         expense.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-    
 
 
 @api_view(['POST'])
